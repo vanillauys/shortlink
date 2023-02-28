88c412d84d54808288ab017700f4e490c2f3648c
Wihan Uys <wihan.uys@gmail.com>
2023-02-28 15:45:53 +0200

    v0.1.0. Frontend mvp. put code that worked where the code that didn't used to be

---
+++ frontend/src/lib/Link.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let title: string;
+    export let date: string;
+    export let fullLink: string;
+    export let shortLink: string;
+</script>
+
+<div class="collapse collapse-arrow border border-base-300 bg-neutral rounded-box">
+	<input type="checkbox" />
+	<div class="collapse-title text-xl font-medium">{shortLink}</div>
+	<div class="collapse-content">
+        <ul>
+            <li>Title: {title}</li>
+            <li>Full link: {fullLink}</li>
+            <li>Date: {date}</li>
+        </ul>
+	</div>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,23 +1,32 @@
 <script lang="ts">
 	import axios from 'axios';
+	import Link from '../lib/Link.svelte';
+	import Error from '../lib/Error.svelte';
 
 	let url: string;
-	let data:any;
+	let data: any = null;
+	let loading: boolean = false;
+	let errMsg: string;
+	let error: boolean = false;
 
 	async function shorten() {
+		loading = true;
 		try {
 			const response = await axios.post(window.location.origin + '/api/shorten', {
 				url: url
 			});
 			data = response.data;
-		} catch (error: any) {
-			console.log(error);
+			loading = false;
+			errMsg = '';
+		} catch (e: any) {
+			error = true;
+			errMsg = e.response.data.detail;
+			loading = false;
 		}
 	}
-
 </script>
 
-<div class="container px-12 py-12 flex flex-col justify-center">
+<div class="container px-12 py-12 flex flex-col justify-center mx-auto">
 	<h1 class="mx-auto text-5xl uppercase font-thin tracking-wide">Shrtn</h1>
 	<p class="text-xl mx-auto mt-6 font-normal">
 		A simple URL shortener built on
@@ -34,11 +43,26 @@
 			bind:value={url}
 		/>
 		<div class="flex justify-center mt-8">
-			<input type="submit" value="shorten it!" class="btn btn-secondary items-center font-normal lowercase" />
+			<input
+				type="submit"
+				value="shorten it!"
+				class="btn btn-secondary items-center font-normal lowercase"
+			/>
 		</div>
 	</form>
+
+	{#if error}
+		<Error errMsg={errMsg} />
+	{/if}
+
 </div>
 
-<div class="container">
-	{data}
+<div class="container mx-auto py-12 px-12">
+	{#if !loading && data != null}	
+		<Link title={data.title} fullLink={data.fullLink} shortLink={data.shortLink} date={data.date} />
+	{:else if loading}
+		<progress class="progress w-2/5 mx-auto py-12"></progress>
+	{:else}
+		<p class="py-12 mx-auto text-center">No links shortened yet...</p>
+	{/if}
 </div>
