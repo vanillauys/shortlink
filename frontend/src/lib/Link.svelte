596195a03839bd0bdaa81de3517dba0e255516cc
Wihan Uys <wihan.uys@gmail.com>
2023-03-01 13:44:19 +0200

    v0.2.2. Delete icon now a X svg! I __ a word

--- frontend/src/lib/Link.svelte
+++ frontend/src/lib/Link.svelte
@@ -23,7 +23,19 @@
 				class="btn btn-error mt-4 w-10 mx-auto text-center"
 				on:click={async () => await delete_link(key)}
 			>
-				<img src="/del.png" class="h-8 w-8 mx-auto p-1 my-auto" alt="Delete" />
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M6 18L18 6M6 6l12 12"
+					/></svg
+				>
 			</button>
 		</div>
 	</div>
